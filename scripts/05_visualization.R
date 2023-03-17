d59087af3cad6cbc8282987f886475e1f25a0d41
sascha-eng <sascha.eng@gmx.ch>
2023-03-17 11:28:52 +0100

    removed unused code added plots for the other variable combinations out of curiosity (won't be saved to output)

--- scripts/05_visualization.R
+++ scripts/05_visualization.R
@@ -12,10 +12,6 @@ library(visreg)
 #load tidyverse
 library(tidyr)
 
-#load the prepared data
-ess_1 <- rio::import(here::here("ess_1.Rda"))
-
-
 #testing for "age_group"
 
 visreg(logitmodel, "age_group",
@@ -28,5 +24,51 @@ visreg(logitmodel, "age_group",
        caption = "source: ESS10")
 ggsave(filename = file.path("output","age_group.png"))
 
+#testing for "eduyrs"
 
+visreg(logitmodel, "eduyrs",
+       gg = TRUE,
+       scale = "response") +
+  labs(y = "Probability to Vote",
+       x = "Age",
+       title = "Relationship of voting and age",
+       subtitle = "controlling for political interest, gender, years of education and region",
+       caption = "source: ESS10")
+
+
+#testing for "polintr"
+
+
+visreg(logitmodel, "polintr",
+       gg = TRUE,
+       scale = "response") +
+  labs(y = "Probability to Vote",
+       x = "Age",
+       title = "Relationship of voting and age",
+       subtitle = "controlling for political interest, gender, years of education and region",
+       caption = "source: ESS10")
+
+
+#testing for "region"
+
+visreg(logitmodel, "region",
+       gg = TRUE,
+       scale = "response") +
+  labs(y = "Probability to Vote",
+       x = "Age",
+       title = "Relationship of voting and age",
+       subtitle = "controlling for political interest, gender, years of education and region",
+       caption = "source: ESS10")
+
+
+#testing for "gndr"
+
+visreg(logitmodel, "gndr",
+       gg = TRUE,
+       scale = "response") +
+  labs(y = "Probability to Vote",
+       x = "Age",
+       title = "Relationship of voting and age",
+       subtitle = "controlling for political interest, gender, years of education and region",
+       caption = "source: ESS10")
 
