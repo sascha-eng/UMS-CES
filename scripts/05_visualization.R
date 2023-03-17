794d10948caba212bc1a02a97f9d4459f157fa30
sascha-eng <sascha.eng@gmx.ch>
2023-03-17 11:00:23 +0100

    added script for visualization of regression

---
+++ scripts/05_visualization.R
@@ -0,0 +1,30 @@
+##Create a visualization of the results
+
+#install and load ggplot2
+install.packages("ggplot2")
+library(ggplot2)
+
+#install and load visreg
+install.packages("visreg")
+library(visreg)
+
+#load tidyverse
+library(tidyr)
+
+#load the prepared data
+ess_1 <- rio::import(here::here("ess_1.Rda"))
+
+
+#testing for "age_group"
+
+visreg(logitmodel, "age_group",
+       gg = TRUE,
+       scale = "response") +
+  labs(y = "Probability to Vote",
+       x = "Age Group",
+       title = "Relationship of voting and age",
+       subtitle = "controlling for political interest, gender, years of education and region",
+       caption = "source: ESS10")
+
+
+
